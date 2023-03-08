b5e178ab9000e05334676f6afebfc34c32ddee13
Neilson Venute <neilsonvenute@gmail.com>
2023-03-07 23:04:59 -0300

    anotações para classes de domínio

--- src/main/java/br/com/venutetech/helpdesk/domain/Cliente.java
+++ src/main/java/br/com/venutetech/helpdesk/domain/Cliente.java
@@ -3,16 +3,26 @@ package br.com.venutetech.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.venutetech.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity()
 public class Cliente extends Pessoa {
 
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/br/com/venutetech/helpdesk/domain/Pessoa.java
+++ src/main/java/br/com/venutetech/helpdesk/domain/Pessoa.java
@@ -1,21 +1,48 @@
 package br.com.venutetech.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import br.com.venutetech.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-public abstract class Pessoa {
+import br.com.venutetech.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public abstract class Pessoa implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
+	
 	protected String nome;
+	
+	@Column(unique = true)
 	protected String cpf;
+	
+	@Column(unique = true)
 	protected String email;
+	
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
 	protected Set <Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 	
 	protected Pessoa() {
