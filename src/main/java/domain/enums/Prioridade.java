423e32b528ec8ff316f5be9e82fcf4697c8e5e73
Neilson Venute <neilsonvenute@gmail.com>
2023-03-07 22:14:48 -0300

    criados enums

--- src/main/java/domain/enums/Prioridade.java
+++ src/main/java/domain/enums/Prioridade.java
@@ -1,7 +1,7 @@
 package domain.enums;
 
 public enum Prioridade {
-	BAIXA(0,"BAIXA"),MEDIA(1,"MEDIA"),ALTA(2,"ALTA");
+	BAIXA(0,"BAIXA"), MEDIA(1,"MEDIA"), ALTA(2,"ALTA");
 	
 	private Integer codigo;
 	private String descricao;
