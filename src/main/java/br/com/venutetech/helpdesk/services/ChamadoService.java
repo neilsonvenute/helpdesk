f47c3521313424460813232d09dc938c9d58268b
Neilson Venute <neilsonvenute@gmail.com>
2023-05-07 22:45:47 -0300

    create para chamado

--- src/main/java/br/com/venutetech/helpdesk/services/ChamadoService.java
+++ src/main/java/br/com/venutetech/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package br.com.venutetech.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,6 +41,13 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return chamadoRepository.save(newChamadoDTO(objDTO));
 	}
+	
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamadoDTO(objDTO);
+		return chamadoRepository.save(oldObj);
+	}
 
 	private Chamado newChamadoDTO(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
@@ -48,6 +56,10 @@ public class ChamadoService {
 		Chamado chamado = new Chamado();
 		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
+		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
 		}
 
 		chamado.setTecnico(tecnico);
